8f798f59bc7e59472866b97933f714fcdfd29865
hugolafis <hugo.newman1@gmail.com>
2025-06-20 07:59:06 +0100

    feat: test data

--- src/classes/Viewer.ts
+++ src/classes/Viewer.ts
@@ -29,6 +29,8 @@ export class Viewer {
 
     const mesh = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshPhysicalMaterial());
     this.scene.add(mesh);
+
+    this.loadTerrainData();
   }
 
   readonly update = (dt: number) => {
@@ -49,4 +51,58 @@ export class Viewer {
 
     this.renderer.render(this.scene, this.camera);
   };
+
+  private async loadTerrainData() {
+    const response = await fetch('./assets/terrain_height.data');
+    const arrayBuffer = await response.arrayBuffer();
+    const pixelArray = new Uint8Array(arrayBuffer);
+
+    const terrainSize = 4096;
+    const vertexCount = terrainSize * 0.25;
+    const divisions = Math.max(1, vertexCount - 1);
+    const geometry = new THREE.PlaneGeometry(terrainSize, terrainSize, divisions, divisions)
+      .rotateX(-Math.PI / 2)
+      .translate(terrainSize * 0.5, 0, terrainSize * 0.5);
+    const material = new THREE.MeshBasicMaterial({ wireframe: true });
+
+    const positionAttrib = geometry.getAttribute('position');
+    const index = geometry.index!;
+
+    const vertex = new THREE.Vector3();
+    for (let i = 0; i < positionAttrib.array.length; i += 3) {
+      vertex.fromArray(positionAttrib.array, i);
+
+      let x = vertex.x;
+      let z = vertex.z;
+
+      x = Math.min(Math.max(x, 0), terrainSize - 1);
+      z = Math.min(Math.max(z, 0), terrainSize - 1);
+
+      const uvIndex = z * terrainSize + x;
+      vertex.y = pixelArray[uvIndex];
+
+      vertex.toArray(positionAttrib.array, i);
+    }
+
+    // for (let i = 0; i < index.array.length; i++) {
+    //   const vertexPositionIndex = index.array[i] * 3;
+    //   vertex.fromArray(positionAttrib.array, vertexPositionIndex);
+
+    //   const x = i % divisions;
+    //   const z = Math.floor(i / divisions);
+
+    //   const pixelIndex = z * terrainSize + x;
+
+    //   // const clampedX = Math.max(0, Math.min(4096 - 1, x));
+    //   // const clampedZ = Math.max(0, Math.min(4096 - 1, z));
+
+    //   vertex.y = pixelArray[pixelIndex];
+    //   vertex.toArray(positionAttrib.array, vertexPositionIndex);
+    // }
+
+    const mesh = new THREE.Mesh(geometry, material);
+    // mesh.scale.x = terrainSize;
+    // mesh.scale.z = terrainSize;
+    this.scene.add(mesh);
+  }
 }
